a708e5c24d06f900759d368bd18c25d785c9ccae
whwogh288 <jaehojo2731@gmail.com>
2022-04-01 00:09:40 +0900

    게시물 리스트 목록 저장

--- src/exam1/Main.java
+++ src/exam1/Main.java
@@ -1,48 +1,40 @@
 package exam1;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("== 프로그램 시작 ==");
-
 		Scanner sc = new Scanner(System.in);
 		
-		int lastArticleId = 0;
-
-		while (true) {
-			System.out.printf("명령어 ) ");
+		List<Integer> nums = new ArrayList<>();
+		
+		while ( true ) {
+			System.out.println("명령어 ) ");
 			String command = sc.nextLine();
-			
 			command = command.trim();
 			
-			if ( command.length() == 0) {
+			
+			if (command.length()== 0) {
 				continue;
 			}
-
-			if (command.equals("system exit")) {
+			
+			if( command.equals("system exit")) {
 				break;
 			}
-			if (command.equals("article write")) {
-				int id = lastArticleId +1;
-				lastArticleId = id;
-				System.out.printf("제목 : ");
-				String title = sc.nextLine();
-				System.out.printf("내용 : ");
-				String body = sc.nextLine();
+			else if (command.equals("num")) {
+				System.out.printf("숫자 : ");
+				int num = sc.nextInt();
+				sc.nextLine();
 				
-				System.out.printf("%d번글이 생성되었습니다.", id);
+				nums.add(num);
 			}
-			else if (command.equals("article list")) {
-				System.out.println("게시물이 없습니다.");
-			}
-			else {
-				System.out.printf("%s(은)는 존재하지 않는 명령어입니다.\n", command);
-
+			else if (command.equals("nums")) {
+				System.out.printf("숫자들 : %s\n", nums.toString());
 			}
 		}
+		
 		sc.close();
-
-		System.out.println("== 프로그램 종료 ==");
 	}
 }
