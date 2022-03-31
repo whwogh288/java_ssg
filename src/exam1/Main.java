1627735427506d874b550266776fdb54dbf424f0
whwogh288 <jaehojo2731@gmail.com>
2022-04-01 00:41:57 +0900

    입력된 게시물들 저장 및 리스트 구현

--- src/exam1/Main.java
+++ src/exam1/Main.java
@@ -6,35 +6,74 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+		
 		Scanner sc = new Scanner(System.in);
 		
-		List<Integer> nums = new ArrayList<>();
+		
+		int lastArticleId = 0;
+		
+		List<Article> articles = new ArrayList<>();
 		
 		while ( true ) {
-			System.out.println("명령어 ) ");
+			System.out.printf("명령어 ) ");
 			String command = sc.nextLine();
 			command = command.trim();
 			
-			
-			if (command.length()== 0) {
+			if (command.length() == 0){
 				continue;
 			}
 			
-			if( command.equals("system exit")) {
+			if (command.equals("system exit")) {
 				break;
 			}
-			else if (command.equals("num")) {
-				System.out.printf("숫자 : ");
-				int num = sc.nextInt();
-				sc.nextLine();
+			
+			if (command.equals("article write")) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
 				
-				nums.add(num);
+				Article article = new Article(id, title, body);
+				articles.add(article);
+				
+				System.out.printf("%d번글이 생성되었습니다.\n", id);
 			}
-			else if (command.equals("nums")) {
-				System.out.printf("숫자들 : %s\n", nums.toString());
+			else if (command.equals("article list")) {
+				if (articles.size() == 0) {
+					System.out.println("게시글이 존재하지 않습니다.");
+					continue;
+				}
+				System.out.println("번호 : 제목");
+				
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					
+					System.out.printf("%d : %s\n", article.id, article.title);
+				}
+				
 			}
+			else {
+				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.");
+			}
+			
 		}
-		
 		sc.close();
+		
+		System.out.println("== 프로그램 끝 ==");
+	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+	
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
 }
