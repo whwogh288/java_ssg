772c907b1053efd7cf91f63f1ace2445c250ca61
whwogh288 <jaehojo2731@gmail.com>
2022-03-31 23:06:19 +0900

    스캐너 연습

---
+++ src/exam1/Main.java
@@ -0,0 +1,17 @@
+package exam1;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String command = sc.nextLine();
+		System.out.printf("입력된 명령어 : %s\n", command);
+		sc.close();
+		
+		System.out.println("== 프로그램 종료 ==");
+	}
+}
